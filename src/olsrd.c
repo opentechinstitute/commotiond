6f611477d67d3ed38882cb288acff4875d192cb9
Josh King <josh@chambana.net>
2013-02-28 11:45:08 -0500

    More OLSRd stubbing.

--- src/olsrd.c
+++ src/olsrd.c
@@ -1,20 +1,12 @@
-/*
- * =====================================================================================
- *
- *       Filename:  olsrd.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  02/28/2013 01:38:33 AM
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (), 
- *   Organization:  
- *
- * =====================================================================================
- */
+#include <stdlib.h>
+#include "extern/list.h"
+#include "debug.h"
+#include "util.h"
+#include "olsrd.h"
+
+process_t olsrd_process_proto = {
+  .init = olsrd_init
+};
 
 static olsrd_conf_item_t global_items[];
 
@@ -24,4 +16,48 @@ static list_t *ifaces;
 
 static list_t *hna;
 
-static int _co_print_global_item_i
+static void _co_olsrd_print_iface_i(list_t *list, lnode_t *lnode, void *context) {
+  olsrd_conf_face_t *iface = lnode_get(lnode);
+
+  //Print to file
+
+  return;
+}
+
+static void _co_olsrd_print_hna_i(list_t *list, lnode_t *lnode, void *context) {
+  olsrd_conf_face_t *iface = lnode_get(lnode);
+
+  //Print to file
+
+  return;
+}
+
+int co_olsrd_add_iface(const char* name, int mode, const char *Ipv4Broadcast, const char *Ipv6Broadcast) {
+  return 1;
+}
+int co_olsrd_remove_iface(const char* name, const int mode, const char *Ipv4Broadcast, const char *Ipv6Broadcast) {
+  return 1;
+}
+
+int co_olsrd_add_hna(const int family, const char *address, const char *netmask) {
+  return 1;
+error:
+  return 0;
+}
+
+int co_olsrd_remove_hna(const int family, const char *address, const char *netmask) {
+  return 1;
+error:
+  return 0;
+}
+
+int co_olsrd_print_conf(const char *filename) {
+  return 1;
+error:
+  return 0;
+}
+
+int olsrd_init(void *self) {
+  olsrd_process_t *this = self;
+  //This function gets called when the process object is created, and should call any initialization stuff that happens before it starts
+}
--- src/olsrd.h
+++ src/olsrd.h
@@ -1,7 +1,15 @@
+#include <stdlib.h>
+#include "process.h"
+#include "util.h"
+
 #define OLSR_HNA4 (1 << 0)
 #define OLSR_HNA6 (1 << 1)
 #define OLSR_IFACE_MESH (1 << 0)
 #define OLSR_IFACE_ETHER (1 << 1)
+
+typedef struct {
+  process_t proto;
+} olsrd_process_t;
 
 typedef struct {
   char *key;
@@ -36,3 +44,5 @@ int co_olsrd_add_hna(const int family, const char *address, const char *netmask)
 int co_olsrd_remove_hna(const int family, const char *address, const char *netmask);
 
 int co_olsrd_print_conf(const char *filename);
+
+int olsrd_init(void *self);
