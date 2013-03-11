7df0ff4a2a7868dee385df480c2ceafbf6083054
Josh King <josh@chambana.net>
2013-03-11 05:59:02 -0400

    Add initial OpenWRT shell bindings for libcommotion.

---
+++ openwrt/lib/functions/commotion.sh
@@ -0,0 +1,171 @@
+#!/bin/sh
+
+#DEBUG=echo
+CLIENT="/usr/bin/commotion"
+SOCKET="/var/run/commotiond.sock"
+
+commotion_get_ip() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface ip 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_netmask() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface netmask 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_ssid() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface ssid 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_bssid() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface bssid 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_channel() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface channel 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_dns() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface dns 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_domain() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface domain 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_wpakey() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface wpakey 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_servald() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface servald 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_mode() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface mode 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_type() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface type 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_nodeid() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET state $iface nodeid 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_get_profile() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET status $iface 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_up() {
+  local iface="$1"
+  local profile="$2"
+  local data=
+
+  data="$($CLIENT -b $SOCKET up $iface $profile 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
+
+commotion_down() {
+  local iface="$1"
+  local data=
+
+  data="$($CLIENT -b $SOCKET up $iface 2>/dev/null)"
+  [[ -z "$data" -o "$?" != 0 ]] || echo "$data" | grep -qs "Failed*" && return 1
+  
+  echo "$data"
+  return 0
+}
