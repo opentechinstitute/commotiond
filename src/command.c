55294594a539a4a3ff8b627d2bc34eb510edf931
Will Hawkins <hawkinsw@opentechinstitute.org>
2013-03-15 13:55:04 -0400

    Use an "IP generation mask"
    Use a special mask to generate IP addresses. This
    is named ipgeneratemask. If none is specified,
    it will fall back to using the netmask field.

--- src/command.c
+++ src/command.c
@@ -145,7 +145,14 @@ char *cmd_up(void *self, char *argv[], int argc) {
 #ifndef _OPENWRT
   co_profile_dump(prof);
   if(!strcmp("true", co_profile_get_string(prof, "ipgenerate", "true"))) {
-    co_generate_ip(co_profile_get_string(prof, "ip", "5.0.0.0"), co_profile_get_string(prof, "netmask", "255.0.0.0"), co_id_get(), address, 0);
+    co_generate_ip(co_profile_get_string(prof, "ip", "5.0.0.0"), 
+      co_profile_get_string(prof, 
+                            "ipgeneratemask",
+                            co_profile_get_string(prof, "netmask", "255.0.0")
+                           ),
+       co_id_get(), 
+       address, 
+       0);
   }
   DEBUG("Address: %s", address);
   if(iface->wireless && co_iface_wpa_connect(iface)) {
@@ -242,9 +249,25 @@ char *cmd_state(void *self, char *argv[], int argc) {
   } else if(!strcmp(argv[1], "ip")) {
     if(!strcmp(co_profile_get_string(prof, "ipgenerate", "true"), "true")) {
       if(!strcmp(co_profile_get_string(prof, "type", "mesh"), "mesh")) {
-        co_generate_ip(co_profile_get_string(prof, "ip", "5.0.0.0"), co_profile_get_string(prof, "netmask", "255.0.0.0"), co_id_get(), address, 0);
+        co_generate_ip(co_profile_get_string(prof, "ip", "5.0.0.0"), 
+                       co_profile_get_string(prof, 
+                                             "ipgeneratemask",
+                                             co_profile_get_string(prof, 
+                                                                   "netmask", 
+                                                                   "255.0.0")),
+                       co_id_get(), 
+                       address, 
+                       0);
       } else {
-        co_generate_ip(co_profile_get_string(prof, "ip", "5.0.0.0"), co_profile_get_string(prof, "netmask", "255.0.0.0"), co_id_get(), address, 1);
+        co_generate_ip(co_profile_get_string(prof, "ip", "5.0.0.0"), 
+                       co_profile_get_string(prof, 
+                                             "ipgeneratemask",
+                                             co_profile_get_string(prof, 
+                                                                   "netmask", 
+                                                                   "255.0.0")),
+                       co_id_get(), 
+                       address, 
+                       1);
       }
       ret = address;
     } else {
