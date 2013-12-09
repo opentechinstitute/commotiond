5bc556ac888fedcb934b781c4468217c7c112690
Josh King <jking@chambana.net>
2013-12-09 14:00:57 -0500

    Add back in core stubs.

---
+++ src/core.h
@@ -0,0 +1,30 @@
+/* vim: set ts=2 expandtab: */
+/**
+ *       @file  core.h
+ *      @brief  Built-in commands and schemas for commotiond.
+ *
+ *     @author  Josh King (jheretic), jking@chambana.net
+ *
+ *   @internal
+ *      Created  12/09/2013 03:02:51 AM
+ *     Compiler  gcc/g++
+ * Organization  The Open Technology Institute
+ *    Copyright  Copyright (c) 2013, Josh King
+ *
+ * This file is part of Commotion, Copyright (c) 2013, Josh King 
+ * 
+ * Commotion is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ * 
+ * Commotion is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Commotion.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * =====================================================================================
+ */
