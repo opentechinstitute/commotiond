4cf6354ca9daa66a94e709430e76697727e6ab80
gradyoti <grady@opentechinstitute.org>
2014-04-01 16:00:58 -0400

    added get/set tests

--- tests/profile.cpp
+++ tests/profile.cpp
@@ -12,38 +12,12 @@
  * int co_profiles_init(const size_t index_size); +
  * co_obj_t *co_profile_find(co_obj_t *name); + 
  * int co_profile_add(const char *name, const size_t nlen); +
- * int co_profile_remove(const char *name, const size_t nlen);
+ * int co_profile_remove(const char *name, const size_t nlen); +
  * co_obj_t *co_profile_get(co_obj_t *profile, const co_obj_t *key);
  * size_t co_profile_get_str(co_obj_t *profile, char **output, const char *key, const size_t klen);
  * int co_profile_set_str(co_obj_t *profile, const char *key, const size_t klen, const char *value, const size_t vlen);
  * 
  * 
- */
-
-/**
- * TEST Create
- * 
- * create
- * init
- * add
- * find
- * 
- * 
- * TEST Modify
- * 
- * create add, etc
- * set string, int, etc.
- * 
- * 
- * TEST Retrieve
- * 
- * create, add, etc.
- * get string, int, etc
- * 
- * 
- * TEST Remove
- * 
- * remove
  */
 
 extern "C" {
@@ -54,6 +28,27 @@ extern "C" {
 }
 #include "gtest/gtest.h"
 
+SCHEMA(default)
+{
+  SCHEMA_ADD("ssid", "commotionwireless.net"); 
+  SCHEMA_ADD("bssid", "02:CA:FF:EE:BA:BE"); 
+  SCHEMA_ADD("bssidgen", "true"); 
+  SCHEMA_ADD("channel", "5"); 
+  SCHEMA_ADD("mode", "adhoc"); 
+  SCHEMA_ADD("type", "mesh"); 
+  SCHEMA_ADD("dns", "208.67.222.222"); 
+  SCHEMA_ADD("domain", "mesh.local"); 
+  SCHEMA_ADD("ipgen", "true"); 
+  SCHEMA_ADD("ip", "100.64.0.0"); 
+  SCHEMA_ADD("netmask", "255.192.0.0"); 
+  SCHEMA_ADD("ipgenmask", "255.192.0.0"); 
+  SCHEMA_ADD("encryption", "psk2"); 
+  SCHEMA_ADD("key", "c0MM0t10n!r0cks"); 
+  SCHEMA_ADD("serval", "false"); 
+  SCHEMA_ADD("announce", "true"); 
+  return 1;
+}
+
 
 class ProfileTest : public ::testing::Test
 {
@@ -63,11 +58,13 @@ protected:
   void Add();
   void Retrieve();
   void Remove();
+  void Get();
   
   // variables
-  int ret;
+  int ret = 0;
   co_obj_t *profile1 = co_str8_create("profile1", 9, 0);
   co_obj_t *profile2 = co_str8_create("profile2", 9, 0);
+  co_obj_t *found = NULL;
   
   // constructor
   ProfileTest()
@@ -103,7 +100,7 @@ void ProfileTest::Retrieve()
   ret = co_profile_add("profile2", 9);
   ASSERT_EQ(1, ret);
 
-  co_obj_t *found = co_profile_find(profile1);
+  found = co_profile_find(profile1);
   ASSERT_TRUE(NULL != found);
   
   found = co_profile_find(profile2);
@@ -122,17 +119,35 @@ void ProfileTest::Remove()
   ASSERT_EQ(1, ret);
   
   // confirm removal
-  co_obj_t *found = co_profile_find(profile1);
+  found = co_profile_find(profile1);
   ASSERT_TRUE(NULL == found);
   
   ret = co_profile_remove("profile2", 9);
   ASSERT_EQ(1, ret);
   
+  //confirm removal
   found = co_profile_find(profile2);
   ASSERT_TRUE(NULL == found);
-  
-  
 }
+
+void ProfileTest::Get()
+{
+  SCHEMA_REGISTER(default);
+  
+  ret = co_profile_add("profile1", 9);
+  ASSERT_EQ(1, ret);
+  
+  found = co_profile_find(profile1);
+  ASSERT_TRUE(NULL != found);
+  
+  ret = co_profile_set_str((co_obj_t *)found, "ip", sizeof("ip"), "192.168.1.254", sizeof("192.168.1.254"));
+  ASSERT_EQ(1, ret);
+  
+//   ret = co_profile_get_str(profile1, &ip, "ip", sizeof("ip");
+//   ASSERT_TRUE(NULL != ret);
+}
+
+
 
 TEST_F(ProfileTest, Init)
 {
@@ -152,4 +167,9 @@ TEST_F(ProfileTest, Retrieve)
 TEST_F(ProfileTest, Remove)
 {
   Remove();
+}
+
+TEST_F(ProfileTest, Get)
+{
+  Get();
 }
