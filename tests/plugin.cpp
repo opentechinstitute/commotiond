b4920de3c7a206d09bacd344d22df4507100f909
gradyoti <grady@opentechinstitute.org>
2014-04-28 13:35:00 -0400

    removed unnecessary includes

--- tests/plugin.cpp
+++ tests/plugin.cpp
@@ -1,25 +1,5 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <getopt.h>
-#include <unistd.h>
-#include <signal.h>
-#include <fcntl.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 extern "C" {
   #include "plugin.h"
-  #include "config.h"
-  #include "debug.h"
-  #include "util.h"
-  #include "loop.h"
-  #include "process.h"
-  #include "profile.h"
-  #include "socket.h"
-  #include "msg.h"
-  #include "iface.h"
-  #include "id.h"
-  #include "list.h"
 }
 #include "gtest/gtest.h"
 
@@ -32,14 +12,12 @@ protected:
   int ret;
 
   // functions
-  
   void Load();
   void Start();
   
   PluginTest()
   {
     co_plugins_init(16);
-    // create plugins list
   }
   
   ~PluginTest()
