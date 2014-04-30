29103af3620931aa1c65676148240d56c105e289
gradyoti <grady@opentechinstitute.org>
2014-04-30 16:54:47 -0400

    added checks to plugin tests

--- tests/plugin.cpp
+++ tests/plugin.cpp
@@ -1,19 +1,33 @@
 extern "C" {
   #include "plugin.h"
+  #include "obj.h"
+  #include "list.h"
 }
 #include "gtest/gtest.h"
 
 #define DEMO_PLUGINDIR "./demolibs"
+
+  
+// iterator function
+co_obj_t *co_context_i(co_obj_t *data, co_obj_t *iter, void *context);
   
 class PluginTest : public ::testing::Test
 {
 protected:
   // variables
   int ret;
+  co_obj_t *plugin1;
+  co_obj_t *_plugins;
+  
+  // NOTE _plugins is a commotion object that becomes the head of the plugins list
 
   // functions
   void Load();
   void Start();
+  void Check();
+  
+  // iterator function
+//   co_obj_t *co_context_i(co_obj_t *data, co_obj_t *iter, void *context);
   
   PluginTest()
   {
@@ -24,6 +38,11 @@ protected:
   {
   }
 };
+
+co_obj_t *co_context_i(co_obj_t *data, co_obj_t *iter, void *context)
+{
+  return data;
+}
 
 void PluginTest::Load()
 {
@@ -38,6 +57,12 @@ void PluginTest::Start()
   ASSERT_EQ(1, ret);
 }
 
+void PluginTest::Check()
+{
+   plugin1 = co_list_parse(_plugins, (co_iter_t)(co_context_i), NULL);
+   DEBUG("\n\n HEY THERE!\n\n");
+}
+
 TEST_F(PluginTest, Load)
 {
   Load();
@@ -46,4 +71,9 @@ TEST_F(PluginTest, Load)
 TEST_F(PluginTest, Start)
 {
   Start();
+}
+
+TEST_F(PluginTest, Check)
+{
+  Check();
 }
