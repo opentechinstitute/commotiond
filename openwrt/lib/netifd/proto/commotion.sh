254886d7e505931e16bb69e4613583b1e84c1029
Josh King <jking@chambana.net>
2014-09-30 16:21:29 -0400

    Fix tabbing.

--- openwrt/lib/netifd/proto/commotion.sh
+++ openwrt/lib/netifd/proto/commotion.sh
@@ -146,7 +146,7 @@ proto_commotion_setup() {
 	      	uci_commit firewall
 	      	set_bridge "$client_bridge" "$iface"
 	      	logger -t "commotion.proto" "Adding $iface to bridge $client_bridge"
-          logger -t "commotion.proto.dhcp" "DHCP type: $dhcp"
+		logger -t "commotion.proto.dhcp" "DHCP type: $dhcp"
 	      	
 	      	logger -t "commotion.proto" "Restarting $client_bridge interface"
 	      	ubus call network.interface."$client_bridge" down
@@ -168,15 +168,15 @@ proto_commotion_setup() {
 	      	
 	      	logger -t "commotion.proto" "Removing $iface from bridge $client_bridge"
 	      	#Can't use unset_bridge here, short-circuits startup process by calling *_teardown
-          brctl delif br-"$client_bridge" "$iface"
+		brctl delif br-"$client_bridge" "$iface"
 	      	proto_export "DHCP_INTERFACE=$config"
-		      logger -t "commotion.proto.dhcp" "DHCP type: $dhcp"
+		logger -t "commotion.proto.dhcp" "DHCP type: $dhcp"
 	      	proto_run_command "$config" udhcpc -i ${iface} -f -T "$dhcp_timeout" -t 0 -p /var/run/udhcpc-"$iface".pid -s /lib/netifd/commotion.dhcp.script
 		return
         ;;
 	"none")
 	      	#Can't use unset_bridge here, short-circuits startup process by calling *_teardown
-		      brctl delif br-"$client_bridge" "$iface"
+		brctl delif br-"$client_bridge" "$iface"
 	;;
       esac
       ;;
@@ -187,7 +187,7 @@ proto_commotion_setup() {
 	if [ $have_ip -eq 0 ]; then
 		if [ "$class" != "mesh" ]; then
 			if [ "$class" == "wired" -a "$dhcp" == "none" ]; then
-        logger -t "commotion.proto.none" "setting ipaddr: $ipaddr and netmask: $netmask"
+				logger -t "commotion.proto.none" "setting ipaddr: $ipaddr and netmask: $netmask"
 				local ip="$ipaddr" 
 				local netmask="$netmask"
 			else 
